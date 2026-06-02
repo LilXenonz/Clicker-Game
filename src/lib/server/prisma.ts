1fb80bb4048e1fa596ff93348ffc915cdc87d8c1
LilXenonz <105544636+LilXenonz@users.noreply.github.com>
2026-06-03 00:49:18 +0200

    deploy

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from './generated/prisma/client.js';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as typeof globalThis & {
 	prisma?: PrismaClient;
