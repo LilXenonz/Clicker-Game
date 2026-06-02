8660f8ce5f2556de211114a51427e087d08c611f
LilXenonz <105544636+LilXenonz@users.noreply.github.com>
2026-06-03 00:46:31 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [sveltekit()] });
+export default defineConfig({
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: [],
+		external: ['@prisma/client', '@prisma/adapter-better-sqlite3', 'better-sqlite3']
+	}
+});
